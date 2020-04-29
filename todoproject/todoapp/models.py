40d4b62b0257b172e5781d017e02e0504cabdcfc
prosoftwaredevelopment <softwaredeveloper1234567890@gmail.com>
2020-04-29 19:41:05 +0100

    todo app initial

---
+++ todoproject/todoapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse,JsonResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def submit(request):
+    obj = Todo()
+    obj.title = request.GET['title']
+    obj.description = request.GET['description']
+    obj.priority = request.GET['priority']
+    obj.save()
+    mydictionary = {
+        "alltodos" : Todo.objects.all()
+    }
+    return render(request,'list.html',context=mydictionary)
+
